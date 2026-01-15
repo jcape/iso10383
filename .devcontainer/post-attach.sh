ab91f9f3742488c85534bd673dae22855404903b
James Cape <jamescape777@pm.me>
2026-01-15 03:21:52 +0000

    ci: update prek test runs

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -3,6 +3,11 @@
 mkdir -p /workspaces/iso10383/.cache/cargo
 ln -sf /usr/local/cargo/bin /workspaces/iso10383/.cache/cargo/
 
+rustup toolchain install stable
+rustup component add --toolchain stable rustfmt
+rustup toolchain install nightly
+rustup component add --toolchain nightly rustfmt
+
 cargo binstall -q -y --force prek
 cargo binstall -q -y --force action-validator
 cargo binstall -q -y --force cargo-deny
--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,6 +3,7 @@
 ARCH=$(arch)
 
 pushd /tmp >/dev/null
+
 curl -qsfL https://github.com/cargo-bins/cargo-binstall/releases/latest/download/cargo-binstall-${ARCH}-unknown-linux-gnu.tgz > /tmp/binstall.tar.gz
 tar -zxf /tmp/binstall.tar.gz
 install -Dpm 0755 /tmp/cargo-binstall /usr/local/cargo/bin/
